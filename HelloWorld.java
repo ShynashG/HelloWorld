4006cec2b537c57e324d25eb01284a4908478c8a
shynash <shynash.gulabjan@quovantis.com>
2021-05-17 20:13:22 +0530

    My Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,33 @@
+
+class HelloWorld {
+    static int first, second, res;
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        add(1,2);
+            
+
+    }
+    public static int add(int a, int b){
+        res = a + b;
+        System.out.println("Sum = " + res);
+        return res; 
+    }
+    
+    public static int sub(int a, int b){
+        res = a -b;
+        System.out.println("sub = " + res);
+        return res; 
+    }
+    
+    public static int mul(int a, int b){
+        res = a*b;
+        System.out.println("Mul = " + res);
+        return res; 
+    }
+    public static int  div(int a, int b){
+        res = a/b;
+        System.out.println("Div = " + res);
+        return res; 
+    }
+}
